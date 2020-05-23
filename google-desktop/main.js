48a07f8b7ffcdcefa1bf495ed3389708c45771f8
Robotica e Desafios <51785898+robotica-desafios@users.noreply.github.com>
2020-05-22 21:19:17 -0300

    Add files via upload

---
+++ google-desktop/main.js
@@ -0,0 +1,35 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      
+    }
+  })
+
+  
+  win.loadURL('https://www.google.com/')
+
+}
+
+
+app.whenReady().then(createWindow)
+
+
+app.on('window-all-closed', () => {
+ 
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
